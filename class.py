32b91e68d04007e813852dcfd58b8a25af2242bf
unknown <haquemahmudul600@gmail.com>
2024-11-06 23:27:05 +0600

    shawon commit 12

--- class.py
+++ class.py
@@ -4,17 +4,27 @@
 # p1 = myclass()
 
 
-class Person:
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+        
+#         print("init exji...")
+#     def __str__(self):
+#         return f" name is {self.name} age is {self.age}"
+
+# p1 = Person("Shawon" , 20)
+
+# p2 = Person("mahmudul" , 19)
+
+# print(p1)
+
+#create a new class
+
+class Animal:
     def __init__(self, name, age):
         self.name = name
         self.age = age
         
-        print("init exji...")
-    def __str__(self):
-        return f" name is {self.name} age is {self.age}"
-
-p1 = Person("Shawon" , 20)
-
-p2 = Person("mahmudul" , 19)
-
-print(p1)
+    def AnimalName(self):
+        return f"Name: {self.name}, Age: {self.age}"
