5e98877126b1df52cb70e35adcd6c866c36de237
unknown <haquemahmudul600@gmail.com>
2024-11-06 23:23:45 +0600

    shawon commit 10

--- class.py
+++ class.py
@@ -1,11 +1,20 @@
+# class myclass:
+#     def __init__(self):
+#         print(10)
+# p1 = myclass()
+
+
 class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+        print("init exji...")
+    def __str__(self):
+        return f" name is {self.name} age is {self.age}"
+        
+p1 = Person("Shawon" , 20)
 
-  def __str__(self):
-    return f"{self.name}({self.age})"    
-
-p1 = Person("John", 36)
+p2 = Person("mahmudul" , 19)
 
 print(p1)
