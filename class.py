ebc607394b13ca7e25bae86e31acc51371550b54
unknown <haquemahmudul600@gmail.com>
2024-11-06 23:16:15 +0600

    shawon commit 4

--- class.py
+++ class.py
@@ -8,8 +8,11 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        
+        print(self)
+        print(self.name)
+        print(self.age)
         print("init exji...")
         
 p1 = Person("Shawon" , 20)
 print(p1.name)
+print(p1)
