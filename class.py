b07e7232f6f8004990075a59e4e7f1afda167078
unknown <haquemahmudul600@gmail.com>
2024-11-06 23:13:45 +0600

    shawon commit 2

--- class.py
+++ class.py
@@ -2,3 +2,10 @@ class myclass:
     def __init__(self):
         print(10)
 p1 = myclass()
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
